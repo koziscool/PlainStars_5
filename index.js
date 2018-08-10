c094196e1b87efc0d341767646ce092c80101145
John Kosmicke <kozjob@yahoo.com>
2018-08-10 06:05:00 -0700

    has click handler setState update does not take function arg

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 
-const Button = function() {
-	return (
-		<button>GO</button>
-	);
+class Button extends React.Component {
+	constructor( props ) {
+		super(props);
+		this.state = { counter: 9 };
+	};
+
+	handleClick = () => {
+		console.log('click', this.state.counter);
+		this.setState( {
+			counter: this.state.counter + 1
+		});
+	};
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>
+				{this.state.counter}
+			</button>
+		);
+	}
 };
+
+
 
 
 ReactDOM.render( <Button/>, root );
