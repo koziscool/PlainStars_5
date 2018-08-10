b16f859c08dd8eb9bdc51d4d80f860fe3dca2155
John Kosmicke <kozjob@yahoo.com>
2018-08-10 06:33:31 -0700

    button component reuse

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ class Button extends React.Component {
 
 	render() {
 		return (
-			<button onClick={this.props.onClickFunction}>
-				+1
+			<button onClick={() => this.props.onClickFunction(this.props.incrementValue)}>
+				+{this.props.incrementValue}
 			</button>
 		);
 	}
@@ -22,10 +22,10 @@ const Result = (props) => {
 class App extends React.Component {
 	state = { counter: 0 };
 
-	incrementCounter = ()  => {
+	incrementCounter = ( incrementValue )  => {
 		this.setState( (prevState) => {
 			return {
-				counter: this.state.counter + 1
+				counter: this.state.counter + incrementValue
 			}
 		});
 	}
@@ -33,8 +33,10 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Button onClickFunction={this.incrementCounter}/>
-		
+				<Button onClickFunction={this.incrementCounter} incrementValue = {1}/>
+				<Button onClickFunction={this.incrementCounter} incrementValue = {5}/>
+				<Button onClickFunction={this.incrementCounter} incrementValue = {10}/>
+				<Button onClickFunction={this.incrementCounter} incrementValue = {100}/>
 				<Result counter={this.state.counter}/>
 			</div>
 		);
