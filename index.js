0112827c8e052f6f78840c07a7db890aa932fd73
John Kosmicke <kozjob@yahoo.com>
2018-08-12 09:46:58 -0700

    hardcoded github user data

--- index.js
+++ index.js
@@ -5,16 +5,27 @@
 
 const Card = (props) => {
 	return (
-		<div> 
-			<img src="http://placehold.it/75" />
-			<div>
-				<div>Name here...</div>			
-				<div>Company Name here...</div>			
+		<div style={{margin: '1em'}}>
+			<img width="75" src="https://avatars1.githubusercontent.com/u/8445?v=4i"/>
+			<div style={{display: 'inline-block', marginLeft: 10}}> 
+				<div>Paul O’Shannessy</div>			
+				<div>Facebook</div>			
 			</div>
 		</div> 
 
 	);
 };
+
+
+const CardList = (props) => {
+	return (
+		<div>
+			<Card />
+		</div>
+	);
+
+};
+
 
 ReactDOM.render( <Card />, root );
 
