812542e3fd6fec2cce494b98bb4a48f396b38d22
John Kosmicke <kozjob@yahoo.com>
2018-08-17 11:59:22 -0700

    refactor cards into App state, get github api data

--- index.js
+++ index.js
@@ -19,21 +19,6 @@ const Card = (props) => {
 };
 
 
-let data = [
-
-	{
-		name:"Paul O'Shanessy",
-		avatar_url:"https://avatars1.githubusercontent.com/u/8445?v=4i",
-		company:"Facebook"
-
-	},
-	{
-		name:"Paul O'Shanessy",
-		avatar_url:"https://avatars1.githubusercontent.com/u/8445?v=4i",
-		company:"Facebook"
-	},
-];
-
 
 const CardList = (props) => {
 	return (
@@ -46,10 +31,31 @@ const CardList = (props) => {
 
 
 class Form extends React.Component{
+
+	state= { userName: '' }
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const theUrl= "https://api.github.com/users/" + this.state.userName;
+		let http = new XMLHttpRequest();
+		http.onreadystatechange = () =>{
+			if( http.readyState === 4 && http.status === 200 ){
+				this.props.onSubmit( JSON.parse( http.responseText ));
+				this.setState( { userName: '' });
+			}
+		}
+
+		http.open( "GET", theUrl, true );
+		http.send( null );
+	};
+
 	render() {
 		return (
-			<form>
-				<input type="text" placeholder="Github username" />
+			<form onSubmit={this.handleSubmit}>
+				<input type="text" 
+					value={ this.state.userName }
+					onChange={ (e)=> this.setState({ userName: e.target.value}) }
+					placeholder="Github username" required/>
 				<button type="submit">Add card</button>
 			</form>
 		);
@@ -58,11 +64,34 @@ class Form extends React.Component{
 }
 
 class App extends React.Component {
+
+	state = { cards: [
+
+		{
+			name:"Paul O'Shanessy",
+			avatar_url:"https://avatars1.githubusercontent.com/u/8445?v=4i",
+			company:"Facebook"
+
+		},
+		{
+			name:"Paul O'Shanessy",
+			avatar_url:"https://avatars1.githubusercontent.com/u/8445?v=4i",
+			company:"Facebook"
+		},
+		]
+	};
+
+
+
+	addNewCard = (cardInfo) => {
+		console.log( cardInfo );
+	};
+
 	render() {
 		return (
 			<div>
-				<Form />
-				<CardList cards={data}/>
+				<Form onSubmit={ this.addNewCard}/>
+				<CardList cards={this.state.cards}/>
 			</div>
 		);
 	}
